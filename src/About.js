ffa1b6a64949a540aede540b8bf234c6f29ed9fd
md Sadique <sadiquekhan08200@example.com>
2024-02-29 21:04:42 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+
+const About = () => {
+  const data = {
+    name: "Quick Shop About",
+  };
+
+  return <HeroSection myData={data} />;
+};
+
+export default About;
